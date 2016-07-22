89310110ad869cd5a2163011aa5594dfeeef807e
maks <maks@debian.asus>
2016-07-22 22:15:42 +0200

    Start

---
+++ bk-mysql-ftp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+bk_host=host
+bk_ftp_user=
+bk_ftp_pass=
+bk_ftp_port=21
+bk_ftp_path=/backup
+log=~/tmp/bk.log
+DATE=$(date)
+# suponeos que usamos .my.cnf si no lo usamos lo creamos... easy... más seguro y universalmente compatible
+if [ ! -d ~/tmp ];then
+mkdir ~/tmp
+fi
+cd ~/tmp
+databases=$(echo "show databases;" | mysql |grep -v performance_schema |grep -v information_schema |grep -v Database)
+
+for bd in $databases
+do
+echo "Respaldando $bd -- $DATE" >> log
+mysqldump $bd > $bd.sql
+echo "Comprimiendo $bd -- $DATE" >> log
+gzip $bd.sql
+echo "Transfiriendo $bd -- $DATE" >> log
+/usr/bin/lftp -c "open -u $bk_ftp_user,$bk_ftp_pass ftp://$bk_host -p $bk_ftp_port; put -O $bk_ftp_path $bd.sql.gz " >> log
+rm -f $bd.sql.gz
+done
+
+
+
