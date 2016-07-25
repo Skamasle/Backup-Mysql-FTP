5efc79be9db65a368531a468a26a89a9b9185bdc
Skamasle <skamasle@gmail.com>
2016-07-25 17:49:27 +0200

    Update bk-mysql-ftp.sh

--- bk-mysql-ftp.sh
+++ bk-mysql-ftp.sh
@@ -14,17 +14,23 @@ mkdir ~/tmp
 fi
 cd ~/tmp
 databases=$(echo "show databases;" | mysql |grep -v performance_schema |grep -v information_schema |grep -v Database)
-
 for bd in $databases
 do
-echo "Respaldando $bd -- $DATE" >> log
-mysqldump $bd > $bd.sql
-echo "Comprimiendo $bd -- $DATE" >> log
+DATE=$(date)
+echo "Respaldando $bd -- $DATE" >> $log
+mysqldump $bd > $bd.sql || rm -f $bd.sql
+if [ -e $bd.sql ];then
+echo "Comprimiendo $bd" >> $log
 gzip $bd.sql
-echo "Transfiriendo $bd -- $DATE" >> log
+echo "Transfiriendo $bd" >> $log
 /usr/bin/lftp -c "open -u $bk_ftp_user,$bk_ftp_pass ftp://$bk_host -p $bk_ftp_port; put -O $bk_ftp_path $bd.sql.gz " >> log
 rm -f $bd.sql.gz
+else
+echo "BD $bd no se a podido respaldar -- $DATE " >> ~/MYSQLDUMP-ERROR.log
+echo "Error al Respaldar $bd" >> $log
+fi
 done
+exit 0
 
 
 
